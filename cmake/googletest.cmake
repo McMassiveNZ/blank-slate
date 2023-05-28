816c7b92a2effb0309fbb585f171d9eee1c607f8
McMassiveNZ <12842622+McMassiveNZ@users.noreply.github.com>
2023-05-28 20:56:49 +0200

    Update googletest.cmake
    Maybe fix the tag?

--- cmake/googletest.cmake
+++ cmake/googletest.cmake
@@ -3,7 +3,7 @@ include_guard()
 CPMAddPackage(
   NAME googletest
   GITHUB_REPOSITORY google/googletest
-  GIT_TAG release-1.13.0
+  GIT_TAG v1.13.0
   VERSION 1.13.0
   OPTIONS
       "INSTALL_GTEST OFF"
